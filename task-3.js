499f992328627cedc655ba9e4febd4201b05fd29
Miras_666 <52910217+mirastalbayev@users.noreply.github.com>
2022-12-05 15:57:24 +0600

    done

--- task-3.js
+++ task-3.js
@@ -1,6 +1,17 @@
 /* Написать функцию JavaScript, которая проверяет,
 является ли переданная строка палиндромом или нет? */
 
+function isPalindrome(str) {
+    let lower = str.toLowerCase();
+    
+    for (let i = 0, j = lower.length - 1; i < j; i++, j--) {
+        if (lower[i] !== lower[j]) {
+            return false;
+        }
+    }
+    return true;
+}
+
 isPalindrome("loki"); // false
 isPalindrome("aba"); // true
 isPalindrome("abab"); // false
