6a9205f1ab204880ba0bd4bf8fb7c64f447a013c
Miras_666 <52910217+mirastalbayev@users.noreply.github.com>
2022-12-05 15:58:02 +0600

    done

--- task-4.js
+++ task-4.js
@@ -2,4 +2,18 @@
 в качестве параметра и преобразует первую букву каждого
 слова строки в верхний регистр. */
 
-uppercaseEachWord("the quick brown fox"); // Быстрая бурая Лиса
+function uppercaseEachWord(str) {
+    let result = "";
+
+    for (let i = 0; i < str.length; i++) {
+        if (i === 0 && str[i] !== ' ' || str[i] !== ' ' && str[i - 1] === ' ') {
+            result += str[i].toUpperCase();
+        } else {
+            result += str[i].toLowerCase();
+        }
+    }
+
+    return result;
+}
+
+uppercaseEachWord("the quick brown fox"); // The Quick Brown Fox
