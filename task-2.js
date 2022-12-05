d38c4cc90ed76b65016e037a58ad52361f74efc8
Miras_666 <52910217+mirastalbayev@users.noreply.github.com>
2022-12-05 15:45:21 +0600

    done

--- task-2.js
+++ task-2.js
@@ -1,4 +1,15 @@
 /* Напишите функцию JavaScript,
 которая преобразует число в обратное. */
 
+function reverse(num) {
+    let transformation = String(num);
+    let result = "";
+
+    for (let i = transformation.length - 1; i >= 0; i--) {
+        result += transformation[i];
+    }
+    
+    return Number(result);
+}
+
 reverse(32234); // 43223
