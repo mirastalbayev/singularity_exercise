9d811b93fcd5801ca1265d105c55cac651c16837
Miras_666 <52910217+mirastalbayev@users.noreply.github.com>
2022-12-05 19:23:44 +0600

    done

--- task-7.js
+++ task-7.js
@@ -1,4 +1,9 @@
 /* Напишите функцию JavaScript для извлечения
 уникальных символов из строки. */
 
+function uniqueChar(text) {
+    let unique = new Set(text);
+    return [...unique].join("");
+}
+
 uniqueChar("thequickbrownfoxjumpsoverthelazydog"); // thequickbrownfxjmpsvlazydg
