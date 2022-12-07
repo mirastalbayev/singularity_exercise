ac0bcdd287f48ed074f59d590d9a30ea2c900a12
Miras_666 <52910217+mirastalbayev@users.noreply.github.com>
2022-12-07 18:52:38 +0600

    done

--- task-9.js
+++ task-9.js
@@ -5,6 +5,12 @@ function Gandalf() {
     console.log("You shall not pass!")
 }
 
+function call(num, func) {
+    for (let i = 0; i < num; i++) {
+        func();
+    }
+}
+
 call(3, Gandalf)
 // You shall not pass!
 // You shall not pass!
