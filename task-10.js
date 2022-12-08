6762d405dfb25d64e39bd421e89410d06659adaa
Miras_666 <52910217+mirastalbayev@users.noreply.github.com>
2022-12-08 19:08:24 +0600

    done

--- task-10.js
+++ task-10.js
@@ -1,6 +1,21 @@
 /* Напишите функцию JavaScript, которая вызывается
 3 раза с числами, кратными 2 наименьшим */
 
-multiply2Max(1)(2)(3); // 2
-multiply2Max(4)(4)(1); // 4
-multiply2Max(3)(0)(5); // 0
+function multiply2Min(a) {
+    return function(b) {
+        return function(c) {
+            if (a % 2 === 0) {
+                return a;
+            } else if (b % 2 === 0) {
+                return b;
+            } else if (c % 2 === 0) {
+                return c;
+            }
+        }
+    }
+}
+
+multiply2Min(1)(2)(3); // 2
+multiply2Min(4)(4)(1); // 4
+multiply2Min(3)(0)(5); // 0
+// поменял название функции multiply2Max на multiply2Min!
